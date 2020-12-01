c30c511f6185c268da115fb6b4cf46dce608f337
Olivier David Laplante <4247532@gmail.com>
2020-12-01 16:28:04 -0500

    Init

--- main.c
+++ main.c
@@ -144,6 +144,7 @@ int ModifierNomItem(item_t* listeItems, short piNbItems, short iItemID, char* sN
  */
 int ModifierDescriptionItem(item_t* listeItems, short piNbItems, short iItemID, char* sNouvelleDescription){
   int iCodeErreur = 0;
+  // Ici
 
   return iCodeErreur;
 }
