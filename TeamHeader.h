9a4cc2584289baab60d0540d61c99c250fda52de
Olivier David Laplante <4247532@gmail.com>
2020-12-01 14:04:10 -0500

    Init

---
+++ TeamHeader.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct item_t {
+  short iID;
+  char* sNom;
+  char* sDescription;
+  int iQuantite;
+} item_t;
---
+++ cmake-build-debug/CMakeFiles/ProjectPige.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjectPige.dir/main.c.obj"
+  "ProjectPige.exe"
+  "ProjectPige.exe.manifest"
+  "ProjectPige.pdb"
+  "libProjectPige.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ProjectPige.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
