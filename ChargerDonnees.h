b2a3aee41ac91ef6a0222ed7f146f35078834d22
Dany Champagne <chopie26_686@hotmail.com>
2020-12-08 07:21:35 -0500

    addon

---
+++ ChargerDonnees.h
@@ -0,0 +1,7 @@
+#ifndef CHARGERDONNEES_H_INCLUDED
+#define CHARGERDONNEES_H_INCLUDED
+#define NETTOYAGE "\t\n\r;"
+
+int ChargerDonnees(item_t*** items, int* iNbItems, char* sNomFichier);
+
+#endif // CHARGERDONNEES_H_INCLUDED
